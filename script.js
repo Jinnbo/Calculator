dec7e240d71f617faf70741a007b7e25d89adde4
Jimmy <jimmyhui92012@gmail.com>
2022-08-18 16:13:12 -0700

    Added Javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+// Button variables
+const ac = document.getElementById('AC');
+const plusMinus = document.getElementById('plusMinus');
+const percent = document.getElementById('percent');
+const divide = document.getElementById('divide');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const multiply = document.getElementById('multiply');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const minus = document.getElementById('minus');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const plus = document.getElementById('plus');
+const zero = document.getElementById('zero');
+const decimal = document.getElementById('decimal');
+const equal = document.getElementById('equal');
+const result = document.getElementById('result');
+
+result.innerHTML = '318008';
